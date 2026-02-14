ad9b3f6984b9d80c578989ac623bcfb49c4d3b7e
GUSTZY CYBER <gustifaiz338@gmail.com>
2026-02-14 14:11:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    ownerNumber: ["6282117447116"], // Format: 628xxx tanpa @s.whatsapp.net
+    ownerNumber: ["6281313876160"], // Format: 628xxx tanpa @s.whatsapp.net
     botName: "VCF AUTO BOT",
     sessionFolder: "./session",
     maxInvitePerMinute: 10,
