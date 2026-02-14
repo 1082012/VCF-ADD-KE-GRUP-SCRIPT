38bbd4269a4e35b8fd141fb37c0f497cd55de782
GUSTZY CYBER <gustifaiz338@gmail.com>
2026-02-14 13:35:41 +0700

    Add configuration settings for VCF AUTO BOT

---
+++ config.js
@@ -0,0 +1,8 @@
+export const config = {
+    ownerNumber: ["6282117447116"], // Format: 628xxx tanpa @s.whatsapp.net
+    botName: "VCF AUTO BOT",
+    sessionFolder: "./session",
+    maxInvitePerMinute: 10,
+    defaultDelay: 5,
+    pairingNumber: "6281313876160" // Nomor bot untuk pairing
+};
